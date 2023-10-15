1a17c2180dff88782453ec91c8ddfc4dbf6b9ff2
mahboubeh <mohtabaiie.m76@gmail.com>
2023-10-15 11:01:36 +0330

    طراحی صفحه درباره ما

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -1,51 +1,63 @@
 <template>
-  <div>
-      <v-container class="text-center">
-        <v-row>
-          <v-col cols="12" md="12">
-            <v-card class="mt-15 text-center py-15 px-10 card_class" >
-              <v-row class="py-10">
-                <v-col cols="12" md="12" class="text_center ">
-                  <h1 >درباره ما</h1>
-                </v-col>
-                <v-col>
-                  <div>
-                    <p class="ma-5">
-                      یک دهه پیش بود که تن تاک متولد شد. برندی برای ایران و
-                      ایرانی، برندی برای خانواده، برندی برای تو و من، برندی برای
-                      همپا بودن
-                    </p>
-                  </div>
-                  <div>
-                    <p class="ma-5 p_matn">
-                      تن تاک با تکیه بر شعار "ما میتوانیم" و اتکاء به دانش
-                      متخصصان ایرانی و تجربه صنعتگران داخلی کشور، اقدام به تولید
-                      بیش از 100 مدل کفش در طرح های چرمی، ورزشی و راحتی برای
-                      تمامی اعضا خانواده های ایرانی نموده است
-                    </p>
-                  </div>
-                  <div>
-                    <p class="ma-5">
-                      این محصولات در بیش از 50 کارگاه تولیدی کوچک و بزرگ در
-                      سراسر میهن عزیزمان تولید می شود و مفتخریم که در راستای
-                      اشتغال جوانان این مرز و بوم قدمی موثر برداشته و در سال
-                      رونق و تولید ملی محیطی آرام و جذاب را در بیش از 120
-                      فروشگاه زنجیره ای تن تاک گرد هم آورده ایم.
-                    </p>
-                  </div>
-                </v-col>
-              </v-row>
-            </v-card>
+  <v-row class="ma-6">
+    <v-col cols="12" md="12">
+      <v-card class="border12" v-if="$vuetify.breakpoint.mdAndUp">
+        <v-breadcrumbs :items="items">
+          <template v-slot:divider>
+            <v-icon>mdi-chevron-left</v-icon>
+          </template>
+        </v-breadcrumbs>
+      </v-card>
+      <v-card class="mt-8 text-center px-10 card_class">
+        <v-row class="py-10">
+          <v-col cols="12" class="d-flex mr-14">
+            <h1>درباره تن تاک</h1>
+          </v-col>
+          <v-col cols="6" md="7" class="text_center">
+            <div class="mx-14">
+              <div class="line-height-40 mx-6">
+                یک دهه پیش بود که تن تاک متولد شد. برندی برای ایران و ایرانی،
+                برندی برای خانواده، برندی برای تو و من، برندی برای همپا بودن
+                <br />
+                تن تاک با تکیه بر شعار "ما میتوانیم" و اتکاء به دانش متخصصان
+                ایرانی و تجربه صنعتگران داخلی کشور، اقدام به تولید بیش از 100
+                مدل کفش در طرح های چرمی، ورزشی و راحتی برای تمامی اعضا خانواده
+                های ایرانی نموده است
+                <br />
+                این محصولات در بیش از 50 کارگاه تولیدی کوچک و بزرگ در سراسر میهن
+                عزیزمان تولید می شود و مفتخریم که در راستای اشتغال جوانان این
+                مرز و بوم قدمی موثر برداشته و در سال رونق و تولید ملی محیطی آرام
+                و جذاب را در بیش از 120 فروشگاه زنجیره ای تن تاک گرد هم آورده
+                ایم.
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="5">
+            <img src="/image/tantak.jpg" width="450" />
           </v-col>
         </v-row>
-      </v-container>
-  </div>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      items: [
+        {
+          text: "خانه",
+          disabled: false,
+          to: "/",
+        },
+        {
+          text: "درباره ما",
+          disabled: true,
+          to: "",
+        },
+      ],
+    };
   },
 };
 </script>
